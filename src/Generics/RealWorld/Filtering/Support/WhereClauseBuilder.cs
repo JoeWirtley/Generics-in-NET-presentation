a09fa083ad3728e0b60781d7c38da82941cd99be
Joe Wirtley <JoeWirtley@users.noreply.github.com>
2015-01-03 09:29:58 -0500

    Added summary comment on where clause builder, including reference to predicate builder.

--- src/Generics/RealWorld/Filtering/Support/WhereClauseBuilder.cs
+++ src/Generics/RealWorld/Filtering/Support/WhereClauseBuilder.cs
@@ -6,6 +6,15 @@ using System.Linq.Expressions;
 namespace Generics.RealWorld.Filtering.Support {
    public delegate Expression CombineUsing( Expression left, Expression right );
 
+   /// <summary>
+   /// Builds a LINQ expression tree from a list of LINQ expression trees, combining them using the 
+   /// delegate passed to the constructor; this delegate will combine using either And or Or.
+   /// </summary>
+   /// <remarks>
+   /// This code is inspired by the predicate builder class from C# 5.0 in a Nutshell, which is a more
+   /// general version of this algorithm.  http://www.albahari.com/nutshell/predicatebuilder.aspx
+   /// </remarks>
+   /// <typeparam name="T"></typeparam>
    public abstract class WhereClauseBuilder<T> {
       private Expression<Func<T, bool>> _whereExpression;
       private readonly CombineUsing _combineUsing;
