d837772e4871e7e3789975b1ce43a8b7c2aaa02d
Joe Wirtley <JoeWirtley@users.noreply.github.com>
2015-01-03 15:24:33 -0500

    Added before example for searialization.

---
+++ src/Generics/RealWorld/Serialization/Before/XmlProcessor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using Generics.RealWorld.Serialization.Support;
+
+namespace Generics.RealWorld.Serialization.Before {
+   public class XmlProcessor {
+
+      public PurchaseOrder DeserializePurchaseOrder( string xml ) {
+         StringReader stringReader = new StringReader( xml );
+         XmlTextReader xmlTextReader = new XmlTextReader( stringReader );
+         XmlSerializer xmlSerializer = new XmlSerializer( typeof( PurchaseOrder ) );
+         return ( ( PurchaseOrder ) ( xmlSerializer.Deserialize( xmlTextReader ) ) );
+      }
+
+      public Invoice DeserializeInvoice( string xml ) {
+         StringReader stringReader = new StringReader( xml );
+         XmlTextReader xmlTextReader = new XmlTextReader( stringReader );
+         XmlSerializer xmlSerializer = new XmlSerializer( typeof( Invoice ) );
+         return ( ( Invoice ) ( xmlSerializer.Deserialize( xmlTextReader ) ) );
+      }
+
+      public string SerializePurchaseOrder( PurchaseOrder po ) {
+         string result = "";
+         MemoryStream serializedStream = new MemoryStream();
+         try {
+            XmlSerializer serializer = new XmlSerializer( typeof( PurchaseOrder ) );
+            XmlTextWriter serializedTextWriter = new XmlTextWriter( serializedStream, new UTF8Encoding() );
+            serializer.Serialize( serializedTextWriter, po );
+            result = UTF8ByteArrayToString( serializedStream.ToArray() );
+         } finally {
+            serializedStream.Close();
+         }
+         return result;
+      }
+
+      public string SerializeInvoice( Invoice invoice ) {
+         string result = "";
+         MemoryStream serializedStream = new MemoryStream();
+         try {
+            XmlSerializer serializer = new XmlSerializer( typeof( Invoice ) );
+            XmlTextWriter serializedTextWriter = new XmlTextWriter( serializedStream, new UTF8Encoding() );
+            serializer.Serialize( serializedTextWriter, invoice );
+            result = UTF8ByteArrayToString( serializedStream.ToArray() );
+         } finally {
+            serializedStream.Close();
+         }
+         return result;
+      }
+
+      private string UTF8ByteArrayToString( Byte[] characters ) {
+         UTF8Encoding encoding = new UTF8Encoding();
+         string constructedString = encoding.GetString( characters );
+         return ( constructedString );
+      }
+   }
+}
